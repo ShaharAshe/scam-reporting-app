7c43172b116f82e3d4dcb97f87a083a1d6bf166c
Yaniv Gabay <yanivga@std.hac.ac.il>
2024-06-14 21:26:36 +0300

    okay, added a common.html for common th.fragments, so easy import bootstrap for each page, can also import the posts controller form everywhere

---
+++ src/main/java/hac/ex5/controller/HomeController.java
@@ -0,0 +1,24 @@
+package hac.ex5.controller;
+
+import hac.ex5.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    private final PostService postService;
+
+    @Autowired
+    public HomeController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping({"/", "/index"})
+    public String showIndex(Model model) {
+        model.addAttribute("posts", postService.getPosts());
+        return "index";  // Ensure this is returning 'index' view
+    }
+}
