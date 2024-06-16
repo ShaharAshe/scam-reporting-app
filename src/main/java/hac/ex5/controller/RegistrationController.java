3e9a3bd5ae12d9882a1a07fad5284b73ed66fd9e
Yaniv Gabay <yanivga@std.hac.ac.il>
2024-06-17 02:17:46 +0300

    seperated the logic, into controller,service,and a dto (data transfer object), registration form will transfer into a user, also working toward creating a test sign controller so it will be easier to register while testing

--- src/main/java/hac/ex5/controller/RegistrationController.java
+++ src/main/java/hac/ex5/controller/RegistrationController.java
@@ -1,5 +1,9 @@
-package hac.ex5.form;
+package hac.ex5.controller;
 
+import hac.ex5.dto.RegistrationForm;
+
+import hac.ex5.dto.TestRegisterForm;
+import hac.ex5.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,28 +16,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import java.util.List;
 
 @Controller
-public class SignUp {
+public class RegistrationController {
+
     @Autowired
-    private FormDataRepository formDataRepository;
+    private UserService userService;
 
     @GetMapping("/signup")
     public String showForm(Model model) {
-        model.addAttribute("formData", new FormData());
+        model.addAttribute("registrationForm", new TestRegisterForm());
         model.addAttribute("genders", List.of("Male", "Female", "Other"));
         return "signup";
     }
 
     @PostMapping("/signup")
-    public String handleFormSubmission(@Valid @ModelAttribute("formData") FormData formData, BindingResult bindingResult, Model model) {
+    public String handleFormSubmission(@Valid @ModelAttribute("registrationForm") RegistrationForm registrationForm, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             model.addAttribute("genders", List.of("Male", "Female", "Other"));
             return "signup";
         }
 
-        // Save form data to the database
-        formDataRepository.save(formData);
-
-        // Process the form data (e.g., save to database)
-        return "Success"; // Redirect to a success page or return a success view
+        userService.registerNewUser(registrationForm);
+        return "redirect:/success";
     }
 }
---
+++ src/main/java/hac/ex5/service/UserService.java
@@ -0,0 +1,12 @@
+package hac.ex5.service;
+
+
+import hac.ex5.dto.RegistrationForm;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public void registerNewUser(RegistrationForm registrationForm) {
+
+    }
+}
