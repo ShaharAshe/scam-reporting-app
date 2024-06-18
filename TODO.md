b4924b3e847ebe8670d163f4eec11d3bd64a8adf
ShaharAshe <shaharas30@gmail.com>
2024-06-18 17:50:48 +0300

    Add to TODO.md file.

--- TODO.md
+++ TODO.md
@@ -4,3 +4,8 @@
 
 1. Block more than one registration to user.
 2. Fix the alert to the registration.
+3. Show all the scam posts to all the users in the website.
+4. Show to each login user it's post in the admin area.
+5. maybe give the option to delete post in the admin area. 
+6. Handle session.
+7. Handle cookies.
--- src/main/java/hac/ex5/controller/HomeController.java
+++ src/main/java/hac/ex5/controller/HomeController.java
@@ -1,6 +1,5 @@
 package hac.ex5.controller;
 
-import hac.ex5.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,16 +10,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class HomeController {
-    private final PostService postService;
-
-    @Autowired
-    public HomeController(PostService postService) {
-        this.postService = postService;
-    }
 
     @GetMapping({"/", "/index"})
     public String showIndex(Model model) {
-        model.addAttribute("posts", postService.getPosts());
+        // model.addAttribute("posts", postService.getPosts());
         return "index";  // Ensure this is returning 'index' view
     }
 
